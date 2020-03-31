bd8faeab500a1bd1111c57bb02ecd443da387a38
Dylan Caldwell <dylan.caldwell35@gmail.com>
2020-03-31 19:22:13 -0400

    Updated Nav Menu

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Projects Links
 const firstLink = () => {
   window.open('https://www.dcmax1k.tk');
 };
@@ -6,9 +7,26 @@ const secLink = () => {
   window.open('https://www.dylancaldwell.tk');
 };
 
+// Menu toggle
 const navTogOpen = document.querySelector('.navTogOpen');
 const navMenu = document.querySelector('.navMenu');
 
 navTogOpen.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+
+// Menu toggle after selecting a link
+
+const navMenuHome = document.querySelector('.navMenu-home');
+const navMenuProjects = document.querySelector('.navMenu-projects');
+const navMenuContact = document.querySelector('.navMenu-contact');
+
+navMenuHome.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+navMenuProjects.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+navMenuContact.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
