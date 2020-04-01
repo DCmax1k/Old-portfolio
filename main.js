aaa8a5bd10014c8715966bb0a57c1bfccc43008e
Dylan Caldwell <dylan.caldwell35@gmail.com>
2020-03-31 20:15:36 -0400

    Added nav toggle animation

--- main.js
+++ main.js
@@ -7,12 +7,21 @@ const secLink = () => {
   window.open('https://www.dylancaldwell.tk');
 };
 
-// Menu toggle
+// Menu toggle/animation
 const navTogOpen = document.querySelector('.navTogOpen');
+const navTogClose = document.querySelector('.navTogClose');
 const navMenu = document.querySelector('.navMenu');
 
 navTogOpen.addEventListener('click', () => {
   navMenu.classList.toggle('active');
+  navTogOpen.classList.toggle('active');
+  navTogClose.classList.toggle('active');
+});
+
+navTogClose.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+  navTogOpen.classList.toggle('active');
+  navTogClose.classList.toggle('active');
 });
 
 // Menu toggle after selecting a link
@@ -23,10 +32,13 @@ const navMenuContact = document.querySelector('.navMenu-contact');
 
 navMenuHome.addEventListener('click', () => {
   navMenu.classList.toggle('active');
+  navTogOpen.classList.toggle('active');
 });
 navMenuProjects.addEventListener('click', () => {
   navMenu.classList.toggle('active');
+  navTogOpen.classList.toggle('active');
 });
 navMenuContact.addEventListener('click', () => {
   navMenu.classList.toggle('active');
+  navTogOpen.classList.toggle('active');
 });
